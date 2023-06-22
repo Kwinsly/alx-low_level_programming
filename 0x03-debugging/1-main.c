e568fe5433fb34f213a6c769337b21ae9e3ab79d
Kwinsly <Queensleyben@gmail.com>
2023-06-22 13:02:19 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,6 @@
 
 
 int main(void)
-
 {
 
 int i;
@@ -18,20 +17,12 @@ printf("Infinite loop incoming :(\n");
 
 
 i = 0;
-
-
 /*while (i < 10)*/
-
 /*{*/
-
 /* putchar(i);*/
-
 /*}*/
-
-
 printf("Infinite loop avoided! \\o/\n");
 
 
 return (0);
-
 }
